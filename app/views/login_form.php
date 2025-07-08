36b518a103cf03a1ff1e6109ac2c371313d17072
Sylvio3365 <sylvioandri@gmail.com>
2025-07-08 19:46:55 +0300

    login

--- app/views/login_form.php
+++ app/views/login_form.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>SignIn</title>
   <link rel="stylesheet" type="text/css" href="/assets/css/login.css" />
-  <!-- <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script> -->
 </head>
+
 <body>
   <div class="container">
     <div class="forms-container">
@@ -16,18 +17,13 @@
           <h2 class="title">Connexion</h2>
           <div class="form-group input-field">
             <i class="fas fa-user"></i>
-            <input type="text" name="identifiant" class="form-control" placeholder="Email ou nom d'utilisateur" required/>
+            <input type="text" name="identifiant" class="form-control" placeholder="Email ou nom d'utilisateur" required />
           </div>
           <div class="form-group input-field">
             <i class="fas fa-lock"></i>
-            <input type="password" name="password" class="form-control" placeholder="Mot de passe" required/>
+            <input type="password" name="password" class="form-control" placeholder="Mot de passe" required />
           </div>
-          <div class="form-group">
-            <div class="custom-control custom-checkbox small" style="line-height: 1.5rem;">
-              <input type="checkbox" class="custom-control-input" id="customCheck">
-              <label class="custom-control-label" for="customCheck">Se souvenir de moi</label>
-            </div>
-          </div>
+          <br>
           <div class="form-group">
             <input type="submit" value="Soumettre" class="btn solid" />
           </div>
@@ -48,4 +44,5 @@
 
   <script src="/assets/js/app.js"></script>
 </body>
+
 </html>
