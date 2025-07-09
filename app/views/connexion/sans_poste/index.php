ff98f4a5f01574598054c19bf140b5c212e6b80f
Tafitia <tafitaran05@gmail.com>
2025-07-09 18:57:38 +0300

    modification des CSS dans certaines pages

--- app/views/connexion/sans_poste/index.php
+++ app/views/connexion/sans_poste/index.php
@@ -12,6 +12,8 @@
     <!-- Select2 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css">
+    <!-- Dark mode support CSS -->
+    <link rel="stylesheet" href="/assets/css/connexion/sans_poste_dark.css">
 </head>
 
 <body class="bg-light">
