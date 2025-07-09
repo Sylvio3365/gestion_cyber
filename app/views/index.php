4754b6e3717221c132b9f22928e939ad1f98066f
Tafitia <tafitaran05@gmail.com>
2025-07-09 14:28:06 +0300

    farany

--- app/views/index.php
+++ app/views/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cyber Pro</title>
+    <title>e-Cyber</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="stylesheet" href="/assets/css/interface-client.css">
